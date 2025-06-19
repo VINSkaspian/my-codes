1e4e50eb431ec65023748c408d591aee0a6baa06
Vins <vinskaspian@gmail.com>
2025-06-19 17:25:48 +0200

    add comments

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
-print("FREE WHISKEY FOR YO DEAR FRIEND!))))") # Запропонований текст 
-answer = input("Do you want some whiskey?") # answer 
-if answer == 'yes': # 
-    name = input("what is yor name? ") #
-    age = int(input("how old are you?")) #
-    if age >=18 : #
-        print("Super! get yor whiskey !)))", name) #
-    else: #
-        print("TO SCHOOL KID!!!!!!") #
-else: # 
-    print("Bye!))))") #
-
+# ЗАГОЛОВОК
+print("FREE WHISKEY FOR YO DEAR FRIEND!))))")
+answer = input("Do you want some whiskey?") #
+# answer умова = присвоєння input (вивід питання в консоль)
+if answer == 'yes':
+# метод if-якщо answer умова == порівняння 'yes':
+    name = input("what is yor name? ")
+# name ім'я = присвоєння input ("вивід питання в консоль ")
+    age = int(input("how old are you?"))
+# age вік = присвоєння int функція що приймає значення або
+# рядок з комою і повертає ціле число(input("вивід питання в консоль "))
+    if age >=18 :
+# метод if-якщо age вік >= більше ніж 18 змінна:
+        print("Super! get yor whiskey !)))", name)
+# print('Вивід повідомлення', name змінна ім'я)
+    else:
+# else: якщо
+        print('GO TO SCHOOL KID!!!!!')
+# print('Вивід повідомлення')
+else:
+# else: якщо
+   print("Bye!))))")
+# print('Вивід повідомлення')
